31bd6ee5c2addbc6e73c380791be2674976fb66c
Matyrobbrt <matyrobbrt@gmail.com>
2023-07-10 19:35:23 +0300

    Update CombiningJavadocProvider.java

--- injector/src/main/java/net/neoforged/javadoctor/injector/CombiningJavadocProvider.java
+++ injector/src/main/java/net/neoforged/javadoctor/injector/CombiningJavadocProvider.java
@@ -22,6 +22,6 @@ public class CombiningJavadocProvider implements JavadocProvider {
                 javadoc = javadoc.merge(provider.get(clazz));
             }
         }
-        return null;
+        return javadoc;
     }
 }
